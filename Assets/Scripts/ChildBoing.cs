d4cccdf809be8fdaa58d0bb396bd13a1a9370566
ryantheking603 <91330332+ryantheking603@users.noreply.github.com>
2025-02-20 16:30:32 -0500

    Child Boing Script

---
+++ Assets/Scripts/ChildBoing.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class ChildBoing : MonoBehaviour
+{
+    AudioSource audioSource;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Trampoline"))
+        {
+            audioSource.Play();
+        }
+    }
+}
